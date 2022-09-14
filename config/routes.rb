05901405cb44bfd9b97d0a6f71bb1dbca097c349
unknown <ibr.s.ahmat@gmail.com>
2022-09-14 19:38:21 +0300

    Change the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
 
   unauthenticated :users do
-    root "users#index"
+    root "users#home"
   end
 
   authenticated :users do
-    root 'groups#index', as: :authenticated_root
+    root 'groups#index', as: ''
   end
 
+  resources :groups, only: [:index]
+
   resources :users do
-    resources :groups, only: [:index] do
+    resources :groups, only: [:index, :new, :show] do
       resources :entities, only: [:index]
     end
   end
