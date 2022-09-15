551ced881d1a82f9b630a7356b6177da214ef9ae
unknown <ibr.s.ahmat@gmail.com>
2022-09-16 00:21:44 +0300

    Add rspec test for the Entity feature

--- spec/features/entity_spec.rb
+++ spec/features/entity_spec.rb
@@ -4,44 +4,29 @@ RSpec.feature 'Entities', type: :feature do
   include Devise::Test::IntegrationHelpers
 
   before(:each) do
-    @user = User.create(name: 'Tester', email: 'test@example.com', password: '123456')
-    @group = Group.create(name: 'Category', icon: 'http://google.com', user_id: @user.id)
-    @entity = Entity.create(name: '')
+    @user = User.create(name: 'Jhon Doe', email: 'jhon@example.com', password: 'password')
+    @group = Group.create(name: 'Category', icon: 'https://google.com', user_id: @user.id)
+    @entity = @group.entities.create(name: 'Transaction', amount: 100, user_id: @user.id)
 
     sign_in @user
   end
 
-  # it 'Page should have (Add a Category) link' do
-  #  visit user_groups_path(@user)
+  it 'Page should have name' do
+    visit user_group_path(@user, @group)
 
-  #  expect(page).to have_content('Add a Category')
-  # end
+    expect(page).to have_content(@group.name)
+  end
 
-  # scenario 'Should go to the new category page' do
-  #  visit user_groups_path(@user)
-  #  first(:link, 'Add a Category').click
+  scenario 'Should go to the new Transaction page' do
+    visit user_group_path(@user, @group)
+    first(:link, 'Add a Transaction').click
 
-  #  expect(page).to have_content('New Category')
-  # end
+    expect(page).to have_content('New Transaction')
+  end
 
-  # scenario 'Should page have (Add a Category) Button' do
-  #  visit user_groups_path(@user)
-  #  first(:link, 'Add a Category').click
+  scenario 'Should page have (Remove) Button' do
+    visit user_groups_path(@user)
 
-  #  expect(page).to have_button('Add a Category')
-  # end
-
-  # scenario 'Should page have (Add a Transaction) Link' do
-  #  visit user_groups_path(@user)
-  #  first(:link, @group.name).click
-
-  #  expect(page).to have_content('Add a Transaction')
-  # end
-
-  # it 'Should page have (Remove) Button' do
-  #  visit user_groups_path(@user)
-  #  first(:link, @group.name).click
-
-  #  expect(page).to have_content('Total:')
-  # end
+    expect(page).to have_button('Remove')
+  end
 end
