32df27fd25e9a5acce32da0aacb6ee9050165f2e
unknown <ibr.s.ahmat@gmail.com>
2022-09-15 21:26:16 +0300

    Add rspec request test for Entity

--- spec/requests/entities_spec.rb
+++ spec/requests/entities_spec.rb
@@ -1,23 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe 'Groups', type: :request do
+RSpec.describe "Entities", type: :request do
   include Devise::Test::IntegrationHelpers
 
-  describe 'Group request test' do
+  describe 'Entity request test' do
     before(:each) do
       @user = User.create(name: 'Tester', email: 'test@gmail.com', password: '12345678')
       @group = Group.create(user: @user, name: 'Category 1', icon: 'http://google.com')
+      @entity = @group.entities.create(user: @user, name: 'transaction 1', amount: 10)
 
       sign_in @user
     end
 
     it 'groups#index - should response status success' do
-      get user_groups_path(@user.id)
+      get user_group_path(@user.id, @group.id)
       expect(response).to have_http_status(:success)
     end
 
-    it 'groups#show - should response status success' do
-      get user_group_path(@user.id, @group.id)
+    it 'entity#new - should response status success' do
+      get new_user_group_entity_path(@user.id, @group.id)
       expect(response).to have_http_status(:success)
     end
   end
