22e28b00adf4b87e27620d25e2618e888d899573
unknown <ibr.s.ahmat@gmail.com>
2022-09-16 01:02:16 +0300

    Fix N+1 problem

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,7 +3,7 @@ class GroupsController < ApplicationController
   load_and_authorize_resource
 
   def index
-    @groups = current_user.groups
+    @groups = current_user.groups.includes(:user)
   end
 
   def show
