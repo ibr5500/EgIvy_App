df004ab57639dc697a03d551b30fd66e7eee5970
unknown <ibr.s.ahmat@gmail.com>
2022-12-12 17:18:06 +0300

    Fix deployment error

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,1 +1,3 @@
-set :runner_command, "rails runner"
+# set :runner_command, "rails runner"
+set :bundle_command, 'bundle exec'
+job_type :runner,  "cd :path && :bundle_command rails runner -e :environment ':task' :output"
